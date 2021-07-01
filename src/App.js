5590d3a637c6779ae1696c6cda7b1c2c6cb0668a
Scott Coupar <scott.coupar@googlemail.com>
2021-07-01 18:40:08 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
