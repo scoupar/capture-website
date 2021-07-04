ca71154b95da4fa28f3ddb9907bba2032eef939d
Scott Coupar <scott.coupar@googlemail.com>
2021-07-04 16:44:04 +0100

    added line animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,6 +5,7 @@ import { About, Description, Image, Hide } from "../styles";
 //Framer motion
 import { motion } from "framer-motion";
 import { TitleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -32,6 +33,7 @@ const AboutSection = () => {
       <Image>
         <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
+      <Wave />
     </About>
   );
 };
